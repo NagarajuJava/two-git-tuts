06da041a5976ea42db3c0ade07e7ba7aed27985c
NagarajuJava <nagaraju.madikanti31@gmail.com>
2023-04-03 17:21:59 +0530

    commiting all 3 files

---
+++ CourseNotFoundException.java
@@ -0,0 +1,14 @@
+package com.ojas.exception;
+
+public class CourseNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public CourseNotFoundException() {
+		super();
+	}
+
+	public CourseNotFoundException(String message) {
+		super(message);
+	}
+}
